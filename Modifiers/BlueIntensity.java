b7c1b23dbac62724326e9fdc0c9c804ffd833757
Kevin Kochpatcharin <k.kochpatcharin@gmail.com>
2016-10-15 15:28:20 -0400

    Update BlueIntensity.java

--- Modifiers/BlueIntensity.java
+++ Modifiers/BlueIntensity.java
@@ -6,12 +6,12 @@ public class BlueIntensity extends Intensity {
     @Override
     protected int modifyPixel(int pixel, float factor) {
         int pixelValue;
-        int[] pixelData = ImageProcessor.toByteData(pixel);
+        int[] pixelData = ImageHelper.toByteData(pixel);
         
         int value = (int) (pixelData[1] * factor);
         pixelData[3] = (byte) (value > 255 ? 255 : value);
         
-        pixelValue = ImageProcessor.fromByteData(pixelData);
+        pixelValue = ImageHelper.fromByteData(pixelData);
         
         return pixelValue;
     }
