d1cdfc960b77625bfa0d64517cf0e44fcba6d024
Kevin Kochpatcharin <k.kochpatcharin@gmail.com>
2016-10-15 14:41:44 -0400

    Create Image.java

---
+++ Image.java
@@ -0,0 +1,13 @@
+/**
+ *
+ * @author Kevin
+ */
+public class Image {
+    public Image(){}
+    public Image(int h, int w) {
+        this.height = h;
+        this.width = w;
+    }
+    public int height, width;
+    public int[] pixels;
+}
