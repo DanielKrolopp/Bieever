4fd17ce5b48e837c2e04b6af43997e6933c1b5f2
Kevin Kochpatcharin <k.kochpatcharin@gmail.com>
2016-10-15 12:41:50 -0400

    Create GlobalModifier.java

---
+++ GlobalModifier.java
@@ -0,0 +1,15 @@
+/**
+ *
+ * @author Kevin
+ */
+public abstract class GlobalModifier {
+    public Image modify(Image img, float factor) {
+        Image newImg = new Image();
+        for (int i = 0; i < img.pixels.length; i++) {
+            newImg.pixels[i] = modifyPixel(img.pixels[i], factor);
+        }
+        return newImg;
+    }
+    
+    public abstract int modifyPixel(int pixel, float factor);
+}
