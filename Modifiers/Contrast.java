14689eb5c5e09567426b29d7d6ea73aec9ad68b4
Kevin Kochpatcharin <k.kochpatcharin@gmail.com>
2016-10-15 22:27:55 -0400

    Update Contrast.java

--- Modifiers/Contrast.java
+++ Modifiers/Contrast.java
@@ -5,9 +5,16 @@
 public class Contrast extends GlobalModifier {
 
     @Override
-    protected int modifyPixel(int pixel, float factor) {
-        int value = (int) (factor * (pixel - 128) + 128);
-        return ImageHelper.clamp(value);
-    }
+    protected int modifyPixel(int pixel, int factor) {
+        double multiplier = 102 * (factor - 100);
+        multiplier /= 100 * (102 - factor);
+        int[] argb = ImageHelper.toByteData(pixel);
+        for (int i = 1; i < argb.length; i++) {
+            argb[i] = (int) (multiplier * (pixel - 128) + 128);
+        }
+        int value = ImageHelper.fromByteData(argb);
+        value = ImageHelper.clamp(value);
+        return value;
+    } 
     
 }
