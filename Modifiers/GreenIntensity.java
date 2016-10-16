df6d7dce39af34a3e35eb102f59ef5847d5d8e50
Kevin Kochpatcharin <k.kochpatcharin@gmail.com>
2016-10-15 23:58:02 -0400

    Update GreenIntensity.java

--- Modifiers/GreenIntensity.java
+++ Modifiers/GreenIntensity.java
@@ -3,9 +3,11 @@
  * @author Kevin
  */
 public class GreenIntensity extends Intensity {
+    public double damping = 40.0;
+    
     @Override
     protected int modifyPixel(int pixel, int factor) {
-        double multiplier = Math.pow(Math.E, factor);
+        double multiplier = Math.pow(Math.E, factor / damping);
         int pixelValue;
         int[] pixelData = ImageHelper.toByteData(pixel);
         
