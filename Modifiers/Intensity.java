f959d0480ad190361dd23b1ca27eed668d9ebfbf
Kevin Kochpatcharin <k.kochpatcharin@gmail.com>
2016-10-15 21:04:21 -0400

    Update Intensity.java

--- Modifiers/Intensity.java
+++ Modifiers/Intensity.java
@@ -6,7 +6,7 @@ public class Intensity extends GlobalModifier {
     @Override
     protected int modifyPixel(int pixel, float factor) {
         int pixelValue;
-        byte[] pixelData = ImageHelper.toByteData(pixel);
+        int[] pixelData = ImageHelper.toByteData(pixel);
         for (int b = 1; b < pixelData.length; b++) {
             int value = (int) (pixelData[b] * factor);
             pixelData[b] = ImageHelper.clamp(value);
