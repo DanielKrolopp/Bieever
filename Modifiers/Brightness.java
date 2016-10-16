ac0f8868663d3ba10b85d29cf85884e20ff32716
Kevin Kochpatcharin <k.kochpatcharin@gmail.com>
2016-10-15 21:27:37 -0400

    Update Brightness.java

--- Modifiers/Brightness.java
+++ Modifiers/Brightness.java
@@ -4,9 +4,18 @@
  */
 public class Brightness extends GlobalModifier {
 
+    /**
+    * 
+    * @param pixel value to be modified
+    * @param factor value of pixel's linear increase in brightness
+    */
     @Override
     protected int modifyPixel(int pixel, float factor) {
-        int value = (int) (pixel + factor);
+        int[] argb = ImageHelper.toByteData(pixel);
+        for (int i = 1; i < argb.length; i++) {
+            argb[i] = (int) (argb[i] + factor + 0.5);
+        }
+        int value = ImageHelper.fromByteData(argb);
         return ImageHelper.clamp(value);
     }
     
