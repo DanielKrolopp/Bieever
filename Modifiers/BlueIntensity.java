df9143173e8532df5853a32da3d28d8390208b78
Kevin Kochpatcharin <k.kochpatcharin@gmail.com>
2016-10-15 21:24:51 -0400

    Update BlueIntensity.java

--- Modifiers/BlueIntensity.java
+++ Modifiers/BlueIntensity.java
@@ -3,6 +3,11 @@
  * @author Kevin
  */
 public class BlueIntensity extends Intensity {
+    /**
+    * 
+    * @param pixel value to be changed
+    * @param factor value that the pixel's blue channel is multiplied by. factor E [0, 1]
+    */
     @Override
     protected int modifyPixel(int pixel, float factor) {
         int pixelValue;
