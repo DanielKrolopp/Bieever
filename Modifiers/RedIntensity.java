a9ed046a901bfa2d4b42a25fdd7c2f4c03a4a349
Kevin Kochpatcharin <k.kochpatcharin@gmail.com>
2016-10-15 22:26:48 -0400

    Update RedIntensity.java

--- Modifiers/RedIntensity.java
+++ Modifiers/RedIntensity.java
@@ -5,10 +5,11 @@
 public class RedIntensity extends Intensity {
     @Override
     protected int modifyPixel(int pixel, float factor) {
+        double multiplier = Math.pow(Math.E, factor);
         int pixelValue;
         int[] pixelData = ImageHelper.toByteData(pixel);
         
-        int value = (int) (pixelData[1] * factor);
+        int value = (int) (pixelData[1] * multiplier);
         pixelData[1] = ImageHelper.clamp(value);
         
         pixelValue = ImageHelper.fromByteData(pixelData);
