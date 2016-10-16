abb5f124a83e7a39d7c76c6615681f74b7a3d284
Kevin Kochpatcharin <k.kochpatcharin@gmail.com>
2016-10-15 20:13:31 -0400

    Create Saturation.java

---
+++ Modifiers/Saturation.java
@@ -0,0 +1,28 @@
+/**
+ *
+ * @author Kevin
+ */
+public class Saturation extends GlobalModifier {
+
+    /**
+     * 
+     * @param pixel
+     * @param factor [0,1]
+     * @return 
+     */
+    @Override
+    protected int modifyPixel(int pixel, float factor) {
+        int[] argb = ImageHelper.toByteData(pixel);
+        double[] hsv = ImageHelper.toHSV(argb);
+        hsv[1] = factor;
+        int[] rgb = ImageHelper.toRGB(hsv);
+        argb[1] = rgb[0];
+        argb[2] = rgb[1];
+        argb[3] = rgb[2];
+        
+        int value = ImageHelper.fromByteData(argb);
+        
+        return value;
+    }
+    
+}
