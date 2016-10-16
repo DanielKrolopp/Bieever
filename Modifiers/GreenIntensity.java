2063296667a9736a3cd29b77930eb87b5292426c
Kevin Kochpatcharin <k.kochpatcharin@gmail.com>
2016-10-15 21:03:54 -0400

    Update GreenIntensity.java

--- Modifiers/GreenIntensity.java
+++ Modifiers/GreenIntensity.java
@@ -6,13 +6,14 @@ public class GreenIntensity extends Intensity {
     @Override
     protected int modifyPixel(int pixel, float factor) {
         int pixelValue;
-        byte[] pixelData = ImageHelper.toByteData(pixel);
+        int[] pixelData = ImageHelper.toByteData(pixel);
         
         int value = (int) (pixelData[2] * factor);
-        pixelData[2] = ImageHelper.clamp(value);
+        pixelData[2] = (byte) (value > 255 ? 255 : value);
         
         pixelValue = ImageHelper.fromByteData(pixelData);
         
         return pixelValue;
     }
 }
+
