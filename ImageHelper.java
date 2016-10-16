f60a3937bc099f82e791f8bc53135829a1b74751
Kevin Kochpatcharin <k.kochpatcharin@gmail.com>
2016-10-15 20:14:16 -0400

    Update ImageHelper.java

--- ImageHelper.java
+++ ImageHelper.java
@@ -97,4 +97,131 @@ public class ImageHelper {
         }
         return bImg;
     }
+       
+    public static int cMaxIndex(int[] argb) {
+        int bigIndex = 1;
+        for (int i = 1; i < argb.length; i++) {
+            if (argb[bigIndex] < argb[i]) {
+                bigIndex = i;
+            }
+        }
+        return bigIndex;
+    }
+    /**
+     * 
+     * @param argb
+     * @return double value of highest RGB value
+     */
+    public static double cMax(int[] argb) {
+        return argb[cMaxIndex(argb)] / 255.0;
+    }
+    
+    public static int cMinIndex(int[] argb) {
+        int smallIndex = 1;
+        for (int i = 1; i < argb.length; i++) {
+            if (argb[smallIndex] > argb[i]) {
+                smallIndex = i;
+            }
+        }
+        return smallIndex;
+    }
+    /**
+     * 
+     * @param argb
+     * @return double value of lowest RGB value E [0, 1]
+     */
+    public static double cMin(int[] argb) {
+        return argb[cMinIndex(argb)] / 255.0;
+    }
+    
+    /**
+     * 
+     * @param argb
+     * @return chroma E [0, 1]
+     */
+    public static double getChroma(int[] argb) {
+        return cMax(argb) - cMin(argb);
+    }
+    
+    /**
+     * 
+     * @param argb
+     * @return hue of RGB values, hue E [0, 360]. if hue == -1, hue is undefined
+     */
+    public static double getHue(int[] argb) {
+        double[] rgb = new double[]{argb[1] / 255.0, argb[2] / 255.0, argb[3] / 255.0};
+        double cMaxIndex = cMaxIndex(argb) - 1;
+        double chroma = getChroma(argb);
+        double hue;
+        if (chroma == 0) return 0;
+        else if (cMaxIndex == 0) {
+            hue = (rgb[1] - rgb[2]) / chroma;
+            hue %= 6;
+        } else if (cMaxIndex == 1) {
+            hue = (rgb[2] - rgb[0]) / chroma + 2;
+        } else {
+            hue = (rgb[0] - rgb[1]) / chroma + 4;
+        }
+        return hue * 60;
+    }
+    
+    public static double getSaturation(int[] argb) {
+        return getChroma(argb) / (cMax(argb));
+    }
+    
+    /** 
+     * 
+     * @param argb
+     * @return {h E [0, 360], s E [0, 1], v E [0, 1]}
+     */
+    public static double[] toHSV(int[] argb) {
+        double[] hsv = new double[]{getHue(argb), getSaturation(argb), cMax(argb)};
+        return hsv;
+    }
+    
+    /**
+     * 
+     * @param hsv {h E [0, 360], s E [0, 1], v E [0, 1]}
+     * @return RGB values from HSV
+     */
+    public static int[] toRGB(double[] hsv) {
+        int[] rgb = new int[3];
+        double[] rgbPrime;
+        
+        double h = hsv[0] / 60;
+        double c = hsv[2] * hsv[1];
+        double x = c * (1 - Math.abs((h % 2) - 1));
+        System.out.println(h + " " + c + " " + x);
+        
+        if (hsv[0] < 0) {
+            rgbPrime = new double[]{0,0,0};
+        } else if (h < 1) {
+            rgbPrime = new double[]{c, x, 0};
+        } else if (h < 2) {
+            rgbPrime = new double[]{x, c, 0};
+        } else if (h < 3) {
+            rgbPrime = new double[]{0, c, x};
+        } else if (h < 4) {
+            rgbPrime = new double[]{0, x, c};
+        } else if (h < 5) {
+            rgbPrime = new double[]{x, 0, c};
+        } else if (h < 6) {
+            rgbPrime = new double[]{c, 0, x};
+        } else {
+            System.out.println("We have a problem in the ImageHelper :3");
+            rgbPrime = null;
+        }
+        
+        rgbPrime[0] += hsv[2] - c;
+        rgbPrime[1] += hsv[2] - c;
+        rgbPrime[2] += hsv[2] - c;
+        
+        System.out.println(rgbPrime[0] + " " + rgbPrime[1] + " " + rgbPrime[2]);
+        
+        rgb[0] = (int) (rgbPrime[0] * 255 + 0.5);
+        rgb[1] = (int) (rgbPrime[1] * 255 + 0.5);
+        rgb[2] = (int) (rgbPrime[2] * 255 + 0.5);
+        
+        return rgb;
+    }
 }
